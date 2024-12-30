0dd7ecc443bbb18a92b4c9dcda2ac7b4ae9dca7d
liudong1 <liudong1@digisky.com>
2024-12-30 21:02:09 +0800

    #Feature first push

---
+++ Source/CustomRenderingPass/Private/ToonOutline/ToonOutlineComponent.cpp
@@ -0,0 +1,34 @@
+﻿#include "ToonOutlineComponent.h"
+
+UToonOutlineComponent::UToonOutlineComponent(const FObjectInitializer& ObjectInitializer)
+{
+#if UE_SERVER
+	AlwaysLoadOnServer = false;
+#else
+	AlwaysLoadOnServer = true;
+#endif
+	
+	AlwaysLoadOnClient = true;
+}
+
+void UToonOutlineComponent::OnAttachmentChanged()
+{
+	Super::OnAttachmentChanged();
+
+	if (auto* SkeletalMesh = Cast<USkeletalMeshComponent>(GetAttachParent()))
+	{
+		SkeletalMeshComponent = MakeWeakObjectPtr(SkeletalMesh);
+	}
+	else
+	{
+		SkeletalMeshComponent.Reset();
+	}
+}
+
+FPrimitiveSceneProxy* UToonOutlineComponent::CreateSceneProxy()
+{
+	if (SkeletalMeshComponent.IsValid())
+		return SkeletalMeshComponent.Get()->GetSceneProxy();
+	
+	return nullptr;
+}
---
+++ Source/CustomRenderingPass/Private/ToonOutline/ToonOutlineComponent.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SkeletalMeshSceneProxy.h"
+#include "ToonOutlineComponent.generated.h"
+
+class FToonOutlineMeshSceneProxy : public FSkeletalMeshSceneProxy
+{
+	FToonOutlineMeshSceneProxy(const USkinnedMeshComponent* Component, FSkeletalMeshRenderData* InSkelMeshRenderData)
+	: FSkeletalMeshSceneProxy(Component, InSkelMeshRenderData)
+	{
+		
+	}
+};
+
+
+/**
+ * 
+ */
+UCLASS(ClassGroup=(ToonLit), meta=(BlueprintSpawnableComponent))
+class UToonOutlineComponent : public UPrimitiveComponent
+{
+	GENERATED_BODY()
+
+public:
+	
+	CUSTOMRENDERINGPASS_API UToonOutlineComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+	virtual void OnAttachmentChanged() override;
+	
+	virtual FPrimitiveSceneProxy* CreateSceneProxy() override;
+
+private:
+
+	UPROPERTY(VisibleInstanceOnly)
+	TWeakObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent;
+};
+
